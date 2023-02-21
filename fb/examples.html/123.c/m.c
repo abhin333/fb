f0d51bae9c682db97b5fce4329b55c7200a5c810
abhin <abhinpradeepan123@gmail.com>
2023-02-20 23:01:12 -0800

    "edit"

--- fb/examples.html/123.c/m.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-    printf("Hello, World!\n");
-    getch(); //Use to get one character input from user, and it will not be printed on screen.
-    return 0;
-}
-html
